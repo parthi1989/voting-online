999138b62ccac8c37a421fad94f47794e394c834
Partheeban Dananjayan <dpartheeban@athenahealth.com>
2018-11-12 16:47:06 +0530

    commit after hbs and heroku

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
 const port = process.env.PORT || 3000;  
-var app = express(); 
-
+var app = express();
+hbs.registerPartials(__dirname+'/views/Partials');
+hbs.registerHelper('getCurrentYear',() => new  Date().getFullYear());
+hbs.registerHelper('screamIT',(text,text2) => text.toUpperCase()+" "+text2.toLowerCase()); 
+app.set('view engine','hbs');
 app.use(express.static(__dirname+'/Public'));
-
+app.use((req,res,next)=>{
+    var now= new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + "\n",(err)=>{ if(err){console.log('unable to append');}});
+    next();
+})
 app.get('/vote',(req,res) =>{
     //res.send(JSON.stringify(req));
-    res.send("Voted successfully");
+    //res.send("Voted successfully");
+    res.render('home.hbs',{PageTitle:"Home Page",BodyContent:"Welcome to Website" ,ABC:"ABC"});
 })
 
 app.listen(port,()=>console.log("Server is up and running in port " + port ));
