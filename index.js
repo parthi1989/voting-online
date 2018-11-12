9df230f745813efc688cad87b9cc1c545b285249
Partheeban Dananjayan <dpartheeban@athenahealth.com>
2018-11-12 12:39:27 +0530

    corrected port as PORT to deploy in heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.port || 3000;  
+const port = process.env.PORT || 3000;  
 var app = express(); 
 
 app.use(express.static(__dirname+'/Public'));
